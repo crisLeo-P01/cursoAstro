6300f5ff3291c6e96f92ffbee81d251f47f26b30
Cristian Paiva <38576658+crisLeo-P01@users.noreply.github.com>
2024-07-26 17:51:12 -0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "htts://mueblesConcept.dev",
+  site: "https://mueblesConcept.dev",
   integrations: [tailwind(), mdx(), react(), sitemap()]
 });
